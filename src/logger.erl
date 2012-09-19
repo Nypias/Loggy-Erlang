756383a62ff0e4cd5da324f3d35492aaf4b6743a
Thomas FATTAL <thomas.fattal@gmail.com>
2012-09-19 18:32:22 +0200

    Logger works

--- src/logger.erl
+++ src/logger.erl
@@ -8,17 +8,83 @@ start(Nodes) ->
 stop(Logger) ->
     Logger ! stop.
 
-init(_) ->
-    loop().
+% Nodes = [paul, ringo, george, john]
+% Creation of lists of list
+% [{john, []}, {george, []}, {ringo, []}, {paul, []}]
+init(Nodes) ->
+    Queue = lists:foldl(fun(X, Acc) -> [{X, []}|Acc] end, [], Nodes),
+    loop(Queue).
 
-loop() ->
+
+loop(Queue) ->
     receive
-	{log, From, Time, Msg} ->
-	    log(From, Time, Msg),
-	    loop();
-	stop -> 
+	{log, From, TimeCurr, Msg} ->
+
+	    % Entry = {john, [{1, Message}, {2, Message}]}
+	    Entry = lists:keyfind(From, 1, Queue),
+
+	    {_, ListEntry} = Entry,    % We add the received message into the list
+	    
+	    case ListEntry of
+		[] ->
+		    NewQueue = lists:keyreplace(From, 1, Queue, {From, ListEntry++[{TimeCurr, Msg}]});
+		    
+		_ ->
+		    %io:format("From : - ~w~n", [From]),
+		    {TimeLast, _} = lists:last(ListEntry),
+		    case TimeCurr > TimeLast of
+			true ->
+			    NewQueue = lists:keyreplace(From, 1, Queue, {From, ListEntry++[{TimeCurr, Msg}]});
+			false ->
+			    NewQueue = Queue
+		    end
+			
+	    end,
+
+	    NextQueue = log(NewQueue),
+	    loop(NextQueue);
+	stop ->
 	    ok
     end.
 
-log(From, Time, Msg) ->
-    io:format("log : ~w ~w ~p~n", [From, Time, Msg]).
+log(Queue) ->
+    %io:format("== LOG == ~n~n"),
+    %io:format('~w~n~n', [Queue]),
+    % We look if all lists are full (one element at least per list)
+    case lists:keyfind([], 2, Queue) of
+	false ->
+
+    % If so, we take the smaller message in one of the list
+	    SmallerMessage = lists:foldl(fun({X, ListX}, Acc) -> 
+						 {CurrTime, Msg} = lists:nth(1,ListX),
+						 case Acc of
+						     {} ->
+							 {X, CurrTime, Msg};
+						     {_,AccTime, _} ->
+							 if CurrTime < AccTime -> {X, CurrTime, Msg};
+							    true -> Acc end
+						 end
+					 end,
+					 {}, Queue),
+
+            % We print the message
+	    {From, Time, Msg} = SmallerMessage,
+	    io:format("log : ~w ~w ~p~n", [From, Time, Msg]),
+	    
+            % We remove from the list
+	    NewQueue = lists:foldl(fun(X, Acc) -> 
+					   {Name, List} = X,
+					   if (Name == From) ->
+						   [{Name,lists:keydelete(Time, 1, List)} | Acc];
+					      true -> [{Name, List} | Acc]
+					   end
+				   end,
+			[], Queue),
+
+            % We call again log()
+	    log(NewQueue);
+	_ -> Queue
+    end.
+
+
+
--- src/worker.erl
+++ src/worker.erl
@@ -12,29 +12,30 @@ init(Name, Log, Seed, Sleep, Jitter) ->
     random:seed(Seed, Seed, Seed),
     receive
 	{peers, Peers} ->
-	    loop(Name, Log, Peers, Sleep, Jitter);
+	    loop(Name, Log, Peers, Sleep, Jitter, 0);
 	stop ->
 	    ok
     end.
 
-loop(Name, Log, Peers, Sleep, Jitter) ->
+loop(Name, Log, Peers, Sleep, Jitter, Counter) ->
     Wait = random:uniform(Sleep),
     receive
 	{msg, Time, Msg} ->
-	    Log ! {log, Name, Time, {received, Msg}},
-	    loop(Name, Log, Peers, Sleep, Jitter);
+	    NewTime = erlang:max(Counter,Time)+1,
+	    Log ! {log, Name, NewTime, {received, Msg}},
+	    loop(Name, Log, Peers, Sleep, Jitter, NewTime);
 	stop ->
 	    ok;
 	Error ->
 	    Log ! {log, Name, time, {error, Error}}
     after Wait ->
 	    Selected = select(Peers),
-	    Time = na,
+	    Time = Counter + 1,
 	    Message = {hello, random:uniform(100)},
-	    Selected ! {msg, Time, message},
+	    Selected ! {msg, Time, Message},
 	    jitter(Jitter),
 	    Log ! {log, Name, Time, {sending, Message}},
-	    loop(Name, Log, Peers, Sleep, Jitter)
+	    loop(Name, Log, Peers, Sleep, Jitter, Time)
     end.
 
 select(Peers) ->
